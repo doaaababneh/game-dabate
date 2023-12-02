355ef72357d92a51ebf6d804378b6210a9fe084e
doaa ababneh <doaaababneh1111@gmail.com>
2023-12-02 10:57:50 +0300

    update

---
+++ src/gamedebate/Signin.java
@@ -0,0 +1,16 @@
+package gamedebate;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Signin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		get.driver
+
+	}
+
+}
